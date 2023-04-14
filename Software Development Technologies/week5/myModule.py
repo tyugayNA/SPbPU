458e4ecd475983bed146dfe46c6895b1b16571c0
Tyugay Nikolay <tyugay@t8.ru>
2023-04-14 10:26:15 +0300

    initial commit. Added 8 weeks of study of Software Development Technology

---
+++ Software Development Technologies/week5/myModule.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class GFG:
+      
+    # methods
+    def add(self, a, b):
+        return a + b
+    def sub(self, a, b):
+        return a - b
+  
+# explicit function      
+def method():
+    print("GFG")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
